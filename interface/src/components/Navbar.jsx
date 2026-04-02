b0dfad4a76b8bfd21b687ef113dcdfd1024e1331
Kalhara-26 <2024t02223@stu.cmb.ac.lk>
2026-04-02 22:01:02 +0530

    Navbar add

--- interface/src/components/Navbar.jsx
+++ interface/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import logoImg from '../assets/Logo.png';
 
 const Navbar = () => {
     return(
-       <header className="flex items-center justify-between px-11 w-full h-10 rounded-b-lg flex items-center bg-linear-to-l to-blue-900 from-blue-200 shadow-2xl">
+       <header className="font-sora flex items-center justify-between px-11 w-full h-15 rounded-b-lg bg-[#021129] shadow-2xl">
 
             <div className='flex items-center gap-0.8 shrink-6'>
                 <img
@@ -14,21 +14,23 @@ const Navbar = () => {
                 <h1 className="block lg:hidden text-lg font-bold text-white/80 ml-4 uppercase tracking-widest">UCMS</h1>
             </div>
 
-            <div className="flex items-center gap-3 md:gap-2 absolute right-55.5">
-                
+            <div className="flex gap-25 ">
+                <div className='flex gap-2'>
                 {/* Profile Icon */}
-                <div className="flex items-center gap-2 relative w-6.5 h-6.5 overflow-hidden bg-blue-100 rounded-full border-2 border-white/30">
+                <div className=" relative w-6.5 h-6.5 overflow-hidden bg-blue-100 rounded-full border-2 border-white/30">
                     <svg className="absolute w-7.5 h-5 text-blue-600 -left-1 top-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path>
                     </svg>
                 </div>
 
-                <h1 className="text-black/65 font-bold font-sans">Maleesha Kalhara</h1>
-            </div>
-
-            <button className="hover:scale-105 transition-colors hover:bg-blue-800 absolute right-5.5 bg-blue-900 rounded-lg text-white/85 hover:cursor-pointer px-5">
+                <h1 className="text-white/60 font-bold">Maleesha Kalhara</h1>
+                </div>
+            <div className='flex items-center'>
+            <button className="hover:scale-105 transition-colors hover:bg-blue-800 absolute right-5.5 bg-blue-900 rounded-lg text-white/85 hover:cursor-pointer p-2">
                 Logout
             </button>
+            </div>
+            </div>
        </header> 
 
     );
