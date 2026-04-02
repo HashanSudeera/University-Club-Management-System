8c4cd97b093dd3e0dbd229d91b6cc60be5c9ed4f
HashanSudeera <hashansudeera380@gmail.com>
2026-04-02 23:00:58 +0530

    add backend

--- server/server.js
+++ server/server.js
@@ -1,15 +1,29 @@
 import express from 'express';
-import { PORT, MONGO_URL } from './config.js';
 import mongoose from 'mongoose';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.js";
+import { PORT, MONGO_URL } from './config.js';
 
 const app = express();
 app.use(express.json());
+app.use(
+    cors({
+        origin: "http://localhost:5173", // Adjust this to your frontend URL
+        credentials: true,
+    })
+);
+app.use(cookieParser());
 
 //mmiddleware
 app.use((req, res, next) => {
     console.log(`Path : ${req.path} | Method : ${req.method}`);
     next();
 })
+
+//ROUTES
+app.use("/api/auth", authRoutes);
 
 //connect Mongo database
 mongoose.connect(MONGO_URL)
