18e4242edd1a26a7e3c281758c7a5e9e77a257a1
Hiruni-Hansini <hirunipremasinghe@gmail.com>
2026-08-19 11:13:19 +0530

    Update models and applying recent changes

--- server/models/Event.js
+++ server/models/Event.js
@@ -2,20 +2,32 @@ const mongoose = require('mongoose');
 
 const EventSchema = new mongoose.Schema({
   //
-  title: { type: String, required: true },
-  date: { type: Date, required: true },
-  location: { type: String },
-  poster_path: { type: String },
-  capacity: { type: Number },
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], //
-  status: { 
-    type: String, 
-    enum: ['Ongoing', 'completed', 'postponed'], //
-    default: 'Ongoing'
-  },
-  type: { 
-    type: String, 
-    enum: ['open', 'register'] //
+
+  event_id:
+             { type: String,
+              unique: true },
+  title:
+             { type: String,
+               required: true },
+  date:
+             { type: Date,
+              required: true },
+  location:
+             { type: String },
+  poster_path:
+             { type: String },
+  capacity:
+             { type: Number },
+  participants:
+             [{ type: mongoose.Schema.Types.ObjectId,
+                ref: 'User' }], //
+  status:
+             { type: String, 
+               enum: ['Ongoing', 'completed', 'postponed'], //
+               default: 'Ongoing'
+    },
+  type:     { type: String, 
+               enum: ['open', 'register'] //
   }
 }, { timestamps: true });
 
--- server/models/EventRegister.js
+++ server/models/EventRegister.js
@@ -2,8 +2,15 @@ const mongoose = require('mongoose');
 
 const EventRegisterSchema = new mongoose.Schema({
   //
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  addons: { type: mongoose.Schema.Types.Mixed } // Flexible type for various addons
+  event_id: 
+        { type: String,
+         unique: true },
+  user_id:
+        { type: mongoose.Schema.Types.ObjectId,
+         ref: 'User',
+         required: true },
+  addons:
+   { type: mongoose.Schema.Types.Mixed } // Flexible type for various addons.
 }, { timestamps: true });
 
 module.exports = mongoose.model('EventRegister', EventRegisterSchema);
